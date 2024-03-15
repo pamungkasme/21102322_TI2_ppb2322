febc0368a8c8d39aa493e08620a1cf34ec000e79
Pamungkasme <m.pamunugkasnanda@gmail.com>
2024-03-15 17:14:43 +0700

    modul 3

--- praktikum_03_mvvm/test/widget_test.dart
+++ praktikum_03_mvvm/test/widget_test.dart
@@ -7,7 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:praktikum_01/main.dart';
+
+import 'package:praktikum_03_mvvm/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
