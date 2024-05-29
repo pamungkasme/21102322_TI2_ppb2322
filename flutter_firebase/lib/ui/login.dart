f8b9f7bc49cdd1f80e7ee2ff2248adc83465f0fd
Pamungkasme <m.pamunugkasnanda@gmail.com>
2024-05-29 18:30:22 +0700

    firebase otp

--- flutter_firebase/lib/ui/login.dart
+++ flutter_firebase/lib/ui/login.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_firebase/bloc/login/login_cubit.dart';
+import 'package:flutter_firebase/ui/home_screen.dart';
+import 'package:flutter_firebase/ui/phone_auth_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../utils/routes.dart';
 
@@ -14,6 +18,20 @@ class _LoginScreenState extends State<LoginScreen> {
   final emailEdc = TextEditingController();
   final passEdc = TextEditingController();
   bool passInvisible = false;
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential).then(
+        (value) async => await Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomeScreen()),
+            (route) => false));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,6 +134,40 @@ class _LoginScreenState extends State<LoginScreen> {
                         fontSize: 24,
                         color: Colors.white),
                   )),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      signInWithGoogle();
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                          'https://img2.pngdownload.id/20190228/qby/kisspng-google-logo-google-account-g-suite-google-images-g-icon-archives-search-png-5c77ad39b77471.9286340315513470017515.jpg'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PhoneAuthScreen()));
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                          'https://freepngimg.com/thumb/business/83615-blue-icons-symbol-telephone-computer-logo.png'),
+                    ),
+                  )
+                ],
+              ),
               SizedBox(
                 height: 25,
               ),
---
+++ flutter_firebase/lib/ui/phone_auth_screen.dart
@@ -0,0 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/ui/home_screen.dart';
+
+class PhoneAuthScreen extends StatefulWidget {
+  const PhoneAuthScreen({super.key});
+
+  @override
+  _PhoneAuthScreenState createState() => _PhoneAuthScreenState();
+}
+
+class _PhoneAuthScreenState extends State<PhoneAuthScreen> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  bool otpVisibility = false;
+  User? user;
+  String verificationID = "";
+
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _otpController = TextEditingController();
+
+  void loginWithPhone() async {
+    auth.verifyPhoneNumber(
+      phoneNumber: "+62${_phoneController.text}",
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await auth.signInWithCredential(credential).then((value) {
+          print("You are logged in successfully");
+        });
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print('Verification failed: ${e.message}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Verification failed: ${e.message}')),
+        );
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        setState(() {
+          otpVisibility = true;
+          verificationID = verificationId;
+        });
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        // Handle timeout
+      },
+    );
+  }
+
+  void verifyOTP() async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationID, smsCode: _otpController.text);
+      await auth.signInWithCredential(credential).then((value) {
+        setState(() {
+          user = FirebaseAuth.instance.currentUser;
+        });
+      }).whenComplete(() {
+        if (user != null) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(const SnackBar(content: Text('Berhasil Login')));
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(const SnackBar(content: Text('Login Gagal')));
+        }
+      });
+    } catch (e) {
+      print('Error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _phoneController.dispose();
+    _otpController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _phoneController,
+              decoration: const InputDecoration(
+                prefix: Padding(padding: EdgeInsets.all(4), child: Text('+62')),
+              ),
+              keyboardType: TextInputType.phone,
+            ),
+            Visibility(
+              visible: otpVisibility,
+              child: TextField(
+                controller: _otpController, // Menggunakan _otpController
+                decoration: const InputDecoration(
+                  hintText: 'OTP',
+                  prefix: Padding(padding: EdgeInsets.all(4), child: Text('')),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              color: const Color(0xff3D4DE0),
+              onPressed: () {
+                if (_phoneController.text.isNotEmpty) {
+                  if (otpVisibility) {
+                    verifyOTP();
+                  } else {
+                    loginWithPhone();
+                  }
+                }
+              },
+              child: Text(otpVisibility ? "Verify" : "Login",
+                  style: const TextStyle(color: Colors.white, fontSize: 20)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
